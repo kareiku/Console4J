2c57b72cf61c84539aed5310c44da48d0eb81c58
kareiku <xaxvu7@gmail.com>
2025-05-13 16:06:39 +0200

    fix: allow for nullable arguments, keeping the argument array non-null

--- src/main/java/io/github/kareiku/IConsole.java
+++ src/main/java/io/github/kareiku/IConsole.java
@@ -1,9 +1,10 @@
 package io.github.kareiku;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public interface IConsole {
-    @NotNull String readLine(@NotNull String fmt, @NotNull Object... args);
+    @NotNull String readLine(@NotNull String fmt, @NotNull Object @Nullable ... args);
 
-    void printf(@NotNull String fmt, @NotNull Object... args);
+    void printf(@NotNull String fmt, @NotNull Object @Nullable ... args);
 }
