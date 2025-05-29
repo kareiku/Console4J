afde290473e5a94ee6226b3dcb4f51e39fbbd718
kareiku <xaxvu7@gmail.com>
2025-05-29 03:01:48 +0200

    refactor: rename method

--- src/main/java/io/github/kareiku/IConsole.java
+++ src/main/java/io/github/kareiku/IConsole.java
@@ -3,5 +3,5 @@ package io.github.kareiku;
 public interface IConsole {
     void printf(String fmt, Object... args);
 
-    String scanf(String fmt, Object... args);
+    String readLine(String fmt, Object... args);
 }
